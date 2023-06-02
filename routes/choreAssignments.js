bd0c2a45678b79e49f2074f23b47dd0bebd16861
wallylime <82895781+wallylime@users.noreply.github.com>
2023-06-02 11:21:14 -0600

    added assignment router

---
+++ routes/choreAssignments.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const choreAssignmentsController = require('../controllers/choreAssignments');
+// const validator = require('../middleware/validate');
+
+router.get('/', choreAssignmentsController.getChoreAssignments);
+router.get('/:id', choreAssignmentsController.getOneChildAssignment);
+router.put('/edit/:id', choreAssignmentsController.editChoreAssignment);
+router.delete('/delete/:id', choreAssignmentsController.deleteChoreAssignment);
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/chores', require('./chores'));
+router.use('/choreAssignments', require('./choreAssignments'));
 router.use('/', require('./swagger'));
 
 module.exports = router;
